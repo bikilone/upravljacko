c6c71c9824c209145d0203ec104fde31c832aad8
bikilone <milosbirovljevic@gmail.com>
2018-11-29 16:33:07 +0100

    added few functionality

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ var leftArrow = document.getElementById("left");
 var rightArrow = document.getElementById("right");
 var bar = document.getElementsByClassName("bar")[0];
 var correct = document.getElementById("correct");
+var points = document.getElementById("points");
+
+var currentPoints = 0;
+
 
 /// setting counter ///
 
@@ -51,10 +55,11 @@ function makeQuestion() {
     h1.innerHTML = "#" + questions[counter][2] + " " + questions[counter][0];
 }
 
-window.onload = function() {
+window.onload = function () {
     haveYouAlreadyAnswerdCorrectly();
     makeQuestion();
     barWidth();
+    points.innerHTML = currentPoints + "/" + counter;
 }
 
 // making answer appear and disappear
@@ -78,19 +83,19 @@ function removeYourAnswer() {
 /// setting bar width
 
 function barWidth() {
-    bar.style.width = (counter + 1)/ questions.length * 100 + "%";
-    bar.innerHTML = Math.floor((counter + 1)/ questions.length * 100) + "%";
+    bar.style.width = (counter + 1) / questions.length * 100 + "%";
+    bar.innerHTML = Math.floor((counter + 1) / questions.length * 100) + "%";
 }
 
 /// setting correct answers in locale storage
 
 var correctAnswers = JSON.parse(localStorage.getItem("Correct")) || [];
 function correctStorage() {
-    
+
     if (counter == questions.length - 1) {
         return
-    } else if (correctAnswers.indexOf(counter) == -1) {
-        correctAnswers.push(counter);
+    } else if (correctAnswers.indexOf(questions[counter][2]) == -1) {
+        correctAnswers.push(questions[counter][2]);
         localStorage.setItem("Correct", JSON.stringify(correctAnswers));
     } else {
         return
@@ -110,18 +115,18 @@ correct.addEventListener("click", function () {
 /// showing only the question you dont know
 
 function haveYouAlreadyAnswerdCorrectly() {
-   questions = questions.filter(function(el, ind) {
+    questions = questions.filter(function (el, ind) {
         if (localStorage.getItem("Correct") && JSON.parse(localStorage.getItem("Correct")).indexOf(el[2]) !== -1) {
             return false;
         } else {
             return true;
         }
-   })
+    })
 }
 
 /// adding event listeners for keypress
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
     /// left arrow
     if (event.which === 37) {
         decreaseCounter();
@@ -135,9 +140,40 @@ document.addEventListener("keydown", function(event) {
         removeAnswer();
         removeYourAnswer();
         barWidth();
+        points.innerHTML = currentPoints + "/" + counter;
     }
     /// enter
     if (event.which === 13) {
         showAnswer();
+        isTheAnswerCorrect();
     }
 })
+
+
+/// checking if answer is correct
+
+function isTheAnswerCorrect() {
+    var yourAnswer = textarea.value.toLowerCase().toString().split(" ");
+    var realAnswer = questions[counter][1].toLowerCase().toString().split(" ");
+    var comperedAnswers = realAnswer.filter(function (el, ind) {
+        if (yourAnswer.indexOf(el) !== -1) {
+            return true;
+        }
+    })
+    var answerRatio = comperedAnswers.length / realAnswer.length;
+    if (answerRatio > 0.4 && answerRatio <= 1) {
+        correctStorage();
+        showAnswer();
+        changePoints();
+    } else {
+        showAnswer();
+    }
+}
+
+// change points
+
+function changePoints() {
+    currentPoints++;
+    var num = counter + 1;
+    points.innerHTML = currentPoints + "/" + num;
+}
